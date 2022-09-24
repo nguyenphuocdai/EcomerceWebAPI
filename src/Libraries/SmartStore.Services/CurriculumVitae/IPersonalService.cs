988fd5902cbf9c2cd42d7d2de18fbfc76e29c339
NguyenPhuocDai <dai.nguyen26091995@gmail.com>
2022-09-24 23:03:51 +0700

    - Added interfaces Personal & PersonalSkill

---
+++ src/Libraries/SmartStore.Services/CurriculumVitae/IPersonalService.cs
@@ -0,0 +1,13 @@
+﻿using SmartStore.Core;
+using SmartStore.Core.Domain.CurriculumVitae;
+
+namespace SmartStore.Services.CurriculumVitae
+{
+    public partial interface IPersonalService
+    {
+        IPagedList<Personal> SearchPersonals(PersonalSearchQuery q);
+        void InsertPersonal(Personal personal);
+        Personal GetPersonalById(int id);
+        void UpdatePersonal(Personal personal);
+    }
+}
---
+++ src/Libraries/SmartStore.Services/CurriculumVitae/IPersonalSkillService.cs
@@ -0,0 +1,17 @@
+﻿using SmartStore.Core;
+using SmartStore.Core.Domain.CurriculumVitae;
+
+namespace SmartStore.Services.CurriculumVitae
+{
+    public partial interface IPersonalSkillService
+    {
+        IPagedList<PersonalSkill> SearchPersonalSkills(PersonalSkillSearchQuery q);
+        void InsertPersonalSkill(PersonalSkill personal);
+        bool IsExistPersonalSkill(int customerId, string skillName);
+        PersonalSkill GetPersonalSkillById(int id);
+        void UpdateSkill(PersonalSkill personalSkill);
+
+        void DeleteSkill(PersonalSkill personalSkill);
+        void DeleteSkill(int id);
+    }
+}
