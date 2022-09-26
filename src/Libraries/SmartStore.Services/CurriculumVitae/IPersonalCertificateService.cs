fac175cc5d0b5b2c2f8b1e0cd8bfdfd02a0a1eac
NguyenPhuocDai <dai.nguyen26091995@gmail.com>
2022-09-26 20:06:49 +0700

    - Added interfaces personal tag & certificate

---
+++ src/Libraries/SmartStore.Services/CurriculumVitae/IPersonalCertificateService.cs
@@ -0,0 +1,16 @@
+﻿using SmartStore.Core;
+using SmartStore.Core.Domain.CurriculumVitae;
+
+namespace SmartStore.Services.CurriculumVitae
+{
+    public partial interface IPersonalCertificateService
+    {
+        IPagedList<PersonalCertificate> SearchCertificates(PersonalCertificateSearchQuery q);
+        void InsertCertificate(PersonalCertificate client);
+        PersonalCertificate GetCertificateById(int id);
+        void UpdateCertificate(PersonalCertificate client);
+        void DeleteCertificate(PersonalCertificate client);
+        void DeleteCertificate(int id);
+
+    }
+}
---
+++ src/Libraries/SmartStore.Services/CurriculumVitae/IPersonalTagService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using SmartStore.Core;
+using SmartStore.Core.Domain.CurriculumVitae;
+
+namespace SmartStore.Services.CurriculumVitae
+{
+    public partial interface IPersonalTagService
+    {
+        IPagedList<PersonalTag> SearchTags(PersonalTagSearchQuery q);
+        void InsertTag(PersonalTag client);
+        bool IsExistTag(string tagName);
+        PersonalTag GetTagById(int id);
+        void UpdateTag(PersonalTag client);
+        void DeleteTag(PersonalTag client);
+        void DeleteTag(int id);
+        IList<string> GetAllTagNames();
+    }
+}
