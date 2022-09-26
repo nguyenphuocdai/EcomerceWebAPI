5a1b4ba446f05e68636aed3bb3289f5e9e3ceba1
NguyenPhuocDai <dai.nguyen26091995@gmail.com>
2022-09-26 20:07:34 +0700

    - Update controller personal portfolio

--- src/Presentation/SmartStore.Web/Administration/Controllers/PersonalPortfolioController.cs
+++ src/Presentation/SmartStore.Web/Administration/Controllers/PersonalPortfolioController.cs
@@ -30,6 +30,7 @@ namespace SmartStore.Admin.Controllers
         private readonly IMediaService _mediaService;
         private readonly MediaSettings _mediaSettings;
         private readonly IPersonalPortfolioService _personalPortfolioService;
+        private readonly IPersonalTagService _personalTagService;
         private readonly ICommonServices _services;
 
         public PersonalPortfolioController(
@@ -39,7 +40,8 @@ namespace SmartStore.Admin.Controllers
             IEventPublisher eventPublisher,
             ICustomerActivityService customerActivityService,
             IMediaService mediaService,
-            MediaSettings mediaSettings)
+            MediaSettings mediaSettings,
+            IPersonalTagService personalTagService)
         {
             _personalPortfolioService = personalPortfolioService;
             _adminAreaSettings = adminAreaSettings;
@@ -48,6 +50,7 @@ namespace SmartStore.Admin.Controllers
             _customerActivityService = customerActivityService;
             _mediaService = mediaService;
             _mediaSettings = mediaSettings;
+            _personalTagService = personalTagService;
         }
 
         [Permission(Permissions.CurriculumVitae.PersonalPortfolio.Read)]
@@ -101,7 +104,7 @@ namespace SmartStore.Admin.Controllers
             model.Published = p.Published;
             model.Status = int.Parse(p.Status);
             model.Description = p.Description;
-            model.Tag = p.Tag;
+            //model.Tag = p.Tag;
             model.ShareLinkFacebook = p.ShareLinkFacebook;
             model.ShareLinkInstagram = p.ShareLinkInstagram;
             model.ShareLinkTwitter = p.ShareLinkTwitter;
@@ -120,51 +123,60 @@ namespace SmartStore.Admin.Controllers
             return View(model);
         }
 
-        //[HttpPost, ParameterBasedOnFormName("save-continue", "continueEditing")]
-        //[ValidateInput(false)]
-        //[ValidateAntiForgeryToken]
-        //[Permission(Permissions.CurriculumVitae.PersonalResume.Create)]
-        //public ActionResult Create(PersonalResumeModel model, bool continueEditing)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        PersonalResume portfolio = new PersonalResume();
-        //        MapModelToClient(model, portfolio);
+        [HttpPost, ParameterBasedOnFormName("save-continue", "continueEditing")]
+        [ValidateInput(false)]
+        [ValidateAntiForgeryToken]
+        [Permission(Permissions.CurriculumVitae.PersonalPortfolio.Create)]
+        public ActionResult Create(PersonalPortfolioModel model, bool continueEditing)
+        {
+            if (ModelState.IsValid)
+            {
+                PersonalPortfolio portfolio = new PersonalPortfolio();
+                MapModelToClient(model, portfolio);
 
-        //        _personalResumeService.InsertResume(portfolio);
-        //        _customerActivityService.InsertActivity("AddNewResume", T("ActivityLog.AddNewResume"), portfolio.Company);
-        //    }
+                _personalPortfolioService.InsertPortfolio(portfolio);
+                _customerActivityService.InsertActivity("AddNewResume", T("ActivityLog.AddNewResume"), portfolio.PortfolioName);
+            }
 
-        //    if (continueEditing)
-        //    {
-        //        return View(model);
-        //    }
+            if (continueEditing)
+            {
+                PreparePortfolioModel(model, null);
+                return View(model);
+            }
 
-        //    NotifySuccess(T("Admin.Catalog.Resume.Added"));
-        //    // If we got this far, something failed, redisplay form.
-        //    PreparePortfolioModel(model, null);
-        //    return RedirectToAction("List");
-        //}
+            NotifySuccess(T("Admin.Catalog.Resume.Added"));
+            // If we got this far, something failed, redisplay form.
+            PreparePortfolioModel(model, null);
+            return RedirectToAction("List");
+        }
 
-        //[NonAction]
-        //protected void MapModelToClient(PersonalResumeModel model, PersonalResume portfolio)
-        //{
-        //    var p = portfolio;
-        //    var m = model;
-        //    var currentTime = DateTime.Now;
+        [NonAction]
+        protected void MapModelToClient(PersonalPortfolioModel model, PersonalPortfolio portfolio)
+        {
+            var p = portfolio;
+            var m = model;
+            var currentTime = DateTime.Now;
 
-        //    p.CustomerId = m.CustomerId;
-        //    p.ResumeType = m.ResumeType.ToString();
-        //    p.ResumeShort = m.ResumeShort;
-        //    p.Company = m.Company;
-        //    p.Address = m.Address;
-        //    p.Title = m.Title;
-        //    p.From = m.From;
-        //    p.To = m.To;
-        //    p.Description = m.Description;
-        //    p.CreatedDate = currentTime;
-        //    p.ModifiedDate = currentTime;
-        //}
+            p.CustomerId = m.CustomerId;
+            p.PortfolioName = m.PortfolioName;
+            p.PortfolioUrl = m.PortfolioUrl;
+            p.ShortMessage = m.ShortMessage;
+            p.From = m.From;
+            p.To = m.To;
+            p.Published = m.Published;
+            p.Status = m.Status.ToString();
+            p.Description = m.Description;
+            if (m.Tag != null)
+            {
+                p.Tag = string.Join(",", m.Tag);
+            }
+            p.ShareLinkFacebook = m.ShareLinkFacebook;
+            p.ShareLinkInstagram = m.ShareLinkInstagram;
+            p.ShareLinkTwitter = m.ShareLinkTwitter;
+
+            p.CreatedDate = currentTime;
+            p.ModifiedDate = currentTime;
+        }
 
         protected void PreparePortfolioModel(PersonalPortfolioModel model, PersonalPortfolio portfolio)
         {
@@ -172,6 +184,9 @@ namespace SmartStore.Admin.Controllers
             Customer currentCustomer = _services.WorkContext.CurrentCustomer;
             if (portfolio == null)
             {
+                var allTags = _personalTagService.GetAllTagNames();
+                model.AvailableProductTags = new MultiSelectList(allTags, model.Tag);
+
                 model.CustomerId = currentCustomer.Id;
                 model.CreatedDate = DateTime.Now;
                 model.ModifiedDate = DateTime.Now;
@@ -187,7 +202,11 @@ namespace SmartStore.Admin.Controllers
                 model.Published = portfolio.Published;
                 model.Status = int.Parse(portfolio.Status);
                 model.Description = portfolio.Description;
-                model.Tag = portfolio.Tag;
+
+                model.Tag = new[] { portfolio.Tag };
+                var allTags = _personalTagService.GetAllTagNames();
+                model.AvailableProductTags = new MultiSelectList(allTags, model.Tag);
+
                 model.ShareLinkFacebook = portfolio.ShareLinkFacebook;
                 model.ShareLinkInstagram = portfolio.ShareLinkInstagram;
                 model.ShareLinkTwitter = portfolio.ShareLinkTwitter;
@@ -197,91 +216,95 @@ namespace SmartStore.Admin.Controllers
             }
         }
 
-        //[Permission(Permissions.CurriculumVitae.PersonalResume.Read)]
-        //public ActionResult Edit(int id)
-        //{
-        //    var portfolio = _personalResumeService.GetResumeById(id);
-        //    if (portfolio == null)
-        //    {
-        //        NotifyWarning(T("Resume.NotFound", id));
-        //        return RedirectToAction("List");
-        //    }
+        [Permission(Permissions.CurriculumVitae.PersonalPortfolio.Read)]
+        public ActionResult Edit(int id)
+        {
+            var portfolio = _personalPortfolioService.GetPortfolioById(id);
+            if (portfolio == null)
+            {
+                NotifyWarning(T("Portfolio.NotFound", id));
+                return RedirectToAction("List");
+            }
 
-        //    var model = portfolio.ToModel();
-        //    PreparePortfolioModel(model, portfolio);
+            var model = portfolio.ToModel();
+            PreparePortfolioModel(model, portfolio);
 
-        //    return View(model);
-        //}
+            return View(model);
+        }
 
-        //[HttpPost, ParameterBasedOnFormName("save-continue", "continueEditing")]
-        //[ValidateInput(false)]
-        //[ValidateAntiForgeryToken]
-        //[Permission(Permissions.CurriculumVitae.PersonalResume.Update)]
-        //public ActionResult Edit(PersonalResumeModel model, bool continueEditing)
-        //{
-        //    var portfolio = _personalResumeService.GetResumeById(model.Id);
-        //    if (portfolio == null)
-        //    {
-        //        NotifyWarning(T("Resume.NotFound", model.Id));
-        //        return RedirectToAction("List");
-        //    }
+        [HttpPost, ParameterBasedOnFormName("save-continue", "continueEditing")]
+        [ValidateInput(false)]
+        [ValidateAntiForgeryToken]
+        [Permission(Permissions.CurriculumVitae.PersonalPortfolio.Update)]
+        public ActionResult Edit(PersonalPortfolioModel model, bool continueEditing)
+        {
+            var portfolio = _personalPortfolioService.GetPortfolioById(model.Id);
+            if (portfolio == null)
+            {
+                NotifyWarning(T("Portfolio.NotFound", model.Id));
+                return RedirectToAction("List");
+            }
 
-        //    if (ModelState.IsValid)
-        //    {
-        //        portfolio.ResumeType = model.ResumeType.ToString();
-        //        portfolio.ResumeShort = model.ResumeShort;
-        //        portfolio.Company = model.Company;
-        //        portfolio.Address = model.Address;
-        //        portfolio.Title = model.Title;
-        //        portfolio.From = model.From;
-        //        portfolio.To = model.To;
-        //        portfolio.Description = model.Description;
-        //        portfolio.ModifiedDate = DateTime.Now;
+            if (ModelState.IsValid)
+            {
+                portfolio.PortfolioName = model.PortfolioName.ToString();
+                portfolio.PortfolioUrl = model.PortfolioUrl;
+                portfolio.ShortMessage = model.ShortMessage;
+                portfolio.From = model.From;
+                portfolio.To = model.To;
+                portfolio.Published = model.Published;
+                portfolio.Status = model.Status.ToString();
+                portfolio.Description = model.Description;
+                portfolio.Tag = string.Join(",", model.Tag);
+                portfolio.ShareLinkFacebook = model.ShareLinkFacebook;
+                portfolio.ShareLinkInstagram = model.ShareLinkInstagram;
+                portfolio.ShareLinkTwitter = model.ShareLinkTwitter;
+                portfolio.ModifiedDate = DateTime.Now;
 
-        //        _personalResumeService.UpdateResume(portfolio);
+                _personalPortfolioService.UpdatePortfolio(portfolio);
 
-        //        _customerActivityService.InsertActivity("EditResume", T("ActivityLog.EditResume"), portfolio.Company);
+                _customerActivityService.InsertActivity("EditResume", T("ActivityLog.EditPortfolio"), portfolio.PortfolioName);
 
-        //        NotifySuccess(T("Admin.Catalog.Resume.Updated"));
-        //        return continueEditing ? RedirectToAction("Edit", new { id = portfolio.Id }) : RedirectToAction("List");
-        //    }
+                NotifySuccess(T("Admin.Catalog.Portfolio.Updated"));
+                return continueEditing ? RedirectToAction("Edit", new { id = portfolio.Id }) : RedirectToAction("List");
+            }
 
-        //    // If we got this far, something failed, redisplay form.
-        //    PreparePortfolioModel(model, portfolio);
+            // If we got this far, something failed, redisplay form.
+            PreparePortfolioModel(model, portfolio);
 
-        //    return View(model);
-        //}
+            return View(model);
+        }
 
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //[Permission(Permissions.CurriculumVitae.PersonalResume.Delete)]
-        //public ActionResult Delete(int id)
-        //{
-        //    var portfolio = _personalResumeService.GetResumeById(id);
-        //    _personalResumeService.DeleteResume(portfolio);
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Permission(Permissions.CurriculumVitae.PersonalPortfolio.Delete)]
+        public ActionResult Delete(int id)
+        {
+            var portfolio = _personalPortfolioService.GetPortfolioById(id);
+            _personalPortfolioService.DeletePortfolio(portfolio);
 
-        //    _customerActivityService.InsertActivity("DeleteProduct", T("ActivityLog.DeleteSkill"), portfolio.Company);
+            _customerActivityService.InsertActivity("DeleteProduct", T("ActivityLog.DeleteSkill"), portfolio.PortfolioName);
 
-        //    NotifySuccess(T("Admin.Catalog.Resume.Deleted"));
-        //    return RedirectToAction("List");
-        //}
+            NotifySuccess(T("Admin.Catalog.Resume.Deleted"));
+            return RedirectToAction("List");
+        }
 
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //[Permission(Permissions.CurriculumVitae.PersonalResume.Delete)]
-        //public ActionResult DeleteSelected(ICollection<int> selectedIds)
-        //{
-        //    if (selectedIds != null)
-        //    {
-        //        foreach (var id in selectedIds)
-        //        {
-        //            _personalResumeService.DeleteResume(id);
-        //        }
-        //    }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Permission(Permissions.CurriculumVitae.PersonalPortfolio.Delete)]
+        public ActionResult DeleteSelected(ICollection<int> selectedIds)
+        {
+            if (selectedIds != null)
+            {
+                foreach (var id in selectedIds)
+                {
+                    _personalPortfolioService.DeletePortfolio(id);
+                }
+            }
 
-        //    NotifySuccess(T("Admin.Common.TaskSuccessfullyProcessed"));
+            NotifySuccess(T("Admin.Common.TaskSuccessfullyProcessed"));
 
-        //    return Json(new { success = true });
-        //}
+            return Json(new { success = true });
+        }
     }
 }
